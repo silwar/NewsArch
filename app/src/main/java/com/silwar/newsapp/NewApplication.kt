22223b8a67d9136f56dc1cc2761ea83892660e62
Yashraj Paralikar <p.yashraj@gmail.com>
2020-01-20 23:02:59 +0530

    Initial commit

---
+++ app/src/main/java/com/silwar/newsapp/NewApplication.kt
@@ -0,0 +1,6 @@
+package com.silwar.newsapp
+
+import android.app.Application
+
+class NewApplication : Application(){
+}
---
+++ app/src/main/java/com/silwar/newsapp/news/NewListViewModel.kt
@@ -0,0 +1,6 @@
+package com.silwar.newsapp.news
+
+import androidx.lifecycle.ViewModel
+
+class NewListViewModel : ViewModel(){
+}
